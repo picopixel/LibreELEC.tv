8e0fb99e78b1911979aba2af5bd8bcead0f3b609
knaerzche <knaerzche@gmail.com>
2020-01-02 16:51:19 +0100

    Update rk uboot to e311da245800596d57b7b7d91ebd4a730747a9ec

--- packages/tools/u-boot/package.mk
+++ packages/tools/u-boot/package.mk
@@ -19,8 +19,8 @@ PKG_NEED_UNPACK="$PROJECT_DIR/$PROJECT/bootloader"
 
 case "$PROJECT" in
   Rockchip)
-    PKG_VERSION="8659d08d2b589693d121c1298484e861b7dafc4f"
-    PKG_SHA256="3f9f2bbd0c28be6d7d6eb909823fee5728da023aca0ce37aef3c8f67d1179ec1"
+    PKG_VERSION="e311da245800596d57b7b7d91ebd4a730747a9ec"
+    PKG_SHA256="a5fd903cfe6255a20f7592ae678bdb3122a6ad83d6a8d47fa44a8cc2988393b5"
     PKG_URL="https://github.com/rockchip-linux/u-boot/archive/$PKG_VERSION.tar.gz"
     PKG_PATCH_DIRS="rockchip"
     PKG_DEPENDS_TARGET+=" rkbin"
