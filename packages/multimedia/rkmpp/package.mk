78447df70b31208f83116bacdecd4aa63e5e7dc8
Alex Bee <knaerzche@gmail.com>
2020-04-27 15:32:48 +0200

    rockchip: rkmpp: update to 4f5e7d3

--- packages/multimedia/rkmpp/package.mk
+++ packages/multimedia/rkmpp/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="rkmpp"
-PKG_VERSION="8b18624b883f71d0c9f98ddc6044ba964a5c607b"
-PKG_SHA256="51c421f5bc39081e46fb5a2f5788996386787515e60fd636fdfbdba04ce9192b"
+PKG_VERSION="4f5e7d3773efd3dbe301e42a934d8462d495fbb9"
+PKG_SHA256="8145ae6441e8071b15d5489a08500d6ca3b3793d58152a525c8c756b451ea13b"
 PKG_ARCH="arm aarch64"
 PKG_LICENSE="APL"
 PKG_SITE="https://github.com/rockchip-linux/mpp"
