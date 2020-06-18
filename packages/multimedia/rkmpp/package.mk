48490f41cce24fbd28816a4451b26f65de6ebef5
Alex Bee <knaerzche@gmail.com>
2020-06-18 22:09:25 +0200

    multimedia: update rkmpp to https://github.com/rockchip-linux/mpp/commit/fefa75939d51f32a70684c5106bd2b28715b4756

--- packages/multimedia/rkmpp/package.mk
+++ packages/multimedia/rkmpp/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="rkmpp"
-PKG_VERSION="4f5e7d3773efd3dbe301e42a934d8462d495fbb9"
-PKG_SHA256="8145ae6441e8071b15d5489a08500d6ca3b3793d58152a525c8c756b451ea13b"
+PKG_VERSION="fefa75939d51f32a70684c5106bd2b28715b4756"
+PKG_SHA256="a4174aa5939a389abf600257eaea970a98fe2fdc7bf5a9440db3fc837936a740"
 PKG_ARCH="arm aarch64"
 PKG_LICENSE="APL"
 PKG_SITE="https://github.com/rockchip-linux/mpp"
